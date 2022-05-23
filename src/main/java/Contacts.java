46403d5039978158a51b754308c396703fa27003
Yosi7708 <102616270+Yosi7708@users.noreply.github.com>
2022-05-23 13:54:16 +0300

    message box element and Contacts added

---
+++ src/main/java/Contacts.java
@@ -0,0 +1,17 @@
+public class Contacts {
+    private String number;
+    private String name;
+
+    public Contacts(String number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
