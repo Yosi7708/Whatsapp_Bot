1a9ea25608642f3ae7b6935bd46fbb38a546cec0
YakovGrintz <Yakov@grintz>
2022-05-23 21:59:44 +0300

    my Class

--- src/main/java/NumberToSendWhatsapp.java
+++ src/main/java/NumberToSendWhatsapp.java
@@ -1,16 +1,48 @@
+import java.util.List;
+
 public class NumberToSendWhatsapp {
     private String _urlToSend;
     private boolean _send;
     public final String URL_OF_WHATSAPP = "https://web.whatsapp.com/send?phone=";
-    public NumberToSendWhatsapp(int number,int prefix){
-        _urlToSend=URL_OF_WHATSAPP+prefix+number;
-        _send=false;
+
+    public NumberToSendWhatsapp(int number, int prefix) {
+        _urlToSend = URL_OF_WHATSAPP + prefix + number;
+        _send = false;
     }
-    public void sent(){
-        _send=true;
+
+    public void sent() {
+        _send = true;
     }
 
     public boolean isSent() {
         return _send;
+    }
+    public String getUrlToSend(){
+        return _urlToSend;
+    }
+
+    public static String whatNotSent(List<NumberToSendWhatsapp> list) {
+        String temp = "";
+        for (NumberToSendWhatsapp x : list        )
+        {
+            if (!x.isSent()){
+                temp= temp + x.getUrlToSend().substring(x.getUrlToSend().indexOf('=')+1)+"\n";
+            }
+
+
+        }
+        return temp;
+    }
+    public static String whatSent(List<NumberToSendWhatsapp> list) {
+        String temp = "";
+        for (NumberToSendWhatsapp x : list        )
+        {
+            if (x.isSent()){
+                temp= temp + x.getUrlToSend().substring(x.getUrlToSend().indexOf('=')+1)+"\n";
+            }
+
+
+        }
+        return temp;
     }
 }
