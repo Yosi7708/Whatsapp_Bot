43a95c3641d7bbdc4d25eb0e75341b45b25c5115
Yosi7708 <102616270+Yosi7708@users.noreply.github.com>
2022-05-23 13:54:16 +0300

    message box element added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,17 +14,25 @@ public class Main {
         scanner.nextLine();
         System.out.println("Enter the message");
         msg = scanner.nextLine();
+
         System.setProperty("webdriver.chrome.driver", "C:\\files\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("user-data-dir=C:\\Users\\tzirkus\\AppData\\Local\\Google\\Chrome\\User Data");
         ChromeDriver driver = new ChromeDriver(options);
         options.addArguments("--start-maximized");
 
-
-        driver.get("https://web.whatsapp.com/send?phone=972" + phone.substring(1) + "&text=" + msg + "&app_absent=0");
-//        driver.get("https://web.whatsapp.com/send?phone=972584464356&text&app_absent=0");
+//     לינק ישיר לשליחת הודעה למספר
+//        driver.get("https://web.whatsapp.com/send?phone=972" + phone.substring(1) + "&text=" + msg + "&app_absent=0");
+        driver.get("https://web.whatsapp.com/send?phone=972"+ phone.substring(1) +"&text&app_absent=0");
         Thread.sleep(4000);
+//        מילוי הודעה ידני - 2 דרכים
+        WebElement messageText = driver.findElement(By.xpath("//div[@id='main']/footer/div/div/span[2]/div/div[2]/div/div/div[2]"));
+        messageText.sendKeys(msg);
 
+//        WebElement element = driver.findElement(By.xpath("//div[@title='הקלדת ההודעה']"));
+//        element.sendKeys("ffff");
+
+        // כפתור שליחה
         WebElement button = driver.findElement(By.xpath("//span[@data-testid=\"send\"]"));
         button.click();
     }
